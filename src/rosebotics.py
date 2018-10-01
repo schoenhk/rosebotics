bb4cf81581d58879d98975730c65b2d156acd721
schoenhk <schoenhk@rose-hulman.edu>
2018-10-01 11:26:54 -0400

    Snathc3r go definition change

--- src/rosebotics.py
+++ src/rosebotics.py
@@ -16,14 +16,13 @@ class StopAction(Enum):
 
 class Snatch3rRobot(object):
     def __init__(self, left_wheel_port=ev3.OUTPUT_B, right_wheel_port=ev3.OUTPUT_C):
-        self.left_motor = Wheel(left_wheel_port)
-        self.right_motor = Wheel(right_wheel_port)
-
+        self.left_wheel = Wheel(left_wheel_port)
+        self.right_wheel = Wheel(right_wheel_port)
 
     def go(self, left_duty_cycle_percent, right_duty_cycle_percent):
-        self.left_motor.start_spinning(left_duty_cycle_percent)
-        self.right_motor.start_spinning(right_duty_cycle_percent)
-        
+        self.left_wheel.start_spinning(left_duty_cycle_percent)
+        self.right_wheel.start_spinning(right_duty_cycle_percent)
+
 
     def stop(self):
         pass
